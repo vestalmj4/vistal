60e3b12370ecfd446dba673a9bfdef45454554d2
vestalmj4 <vestalmj4@gmail.com>
2024-11-26 16:35:34 +0800

    Update Editable_Biography_app.py

--- Editable_Biography_app.py
+++ Editable_Biography_app.py
@@ -7,7 +7,7 @@ st.title("Biography of Marjon P. Vistal")
 st.markdown("""
     <style>
         .circle-img {
-            border-radius: 50%;  /* Changed to 50% for a perfect circle */
+            border-radius: 25%;  /* Changed to 50% for a perfect circle */
             width: 300px;  /* You can adjust the size here */
             height: 300px;  /* Make sure it's square to maintain the circular shape */
             object-fit: cover;  /* Ensures the image fits well inside the circle */
